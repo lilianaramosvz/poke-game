4708ee4d61ed5e27cc84a7768f905e03d4e00852
delsa21 <A01644911@tec.mx>
2026-03-27 00:16:54 -0600

    Corrección backend y frontend

--- frontend/src/firebase.js
+++ frontend/src/firebase.js
@@ -1,20 +1,16 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { getAuth } from "firebase/auth";
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDiD6EXLHIb3lWG8N9zSgPmHrIdzkHdZTk",
-  authDomain: "poke-game-bced1.firebaseapp.com",
-  projectId: "poke-game-bced1",
-  storageBucket: "poke-game-bced1.firebasestorage.app",
-  messagingSenderId: "953104484880",
-  appId: "1:953104484880:web:6f5da76a55f4d030f79895",
-  measurementId: "G-88LWB5JCG7"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  databaseURL: import.meta.env.VITE_FIREBASE_DATABASE_URL,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+
